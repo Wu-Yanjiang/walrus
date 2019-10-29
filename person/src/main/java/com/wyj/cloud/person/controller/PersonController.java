47288a285ae7a4bea26cf6286fd633ee264565cb
Wu Yanjiang <junglew@qq.com>
2019-10-29 23:47:16 +0800

    init project

---
+++ person/src/main/java/com/wyj/cloud/person/controller/PersonController.java
@@ -0,0 +1,19 @@
+package com.wyj.cloud.person.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @Description:
+ * @Author: Jungle.Wu
+ * @Date: 2019/10/29
+ * @email: wuyj_prom@si-tech.com.cn
+ */
+@Controller
+public class PersonController {
+
+    public List<Person> savePerson(@RequestBody String persopnName) {
+
+    }
+}
---
+++ person/src/main/java/com/wyj/cloud/person/entity/Person.java
@@ -0,0 +1,14 @@
+package com.wyj.cloud.person.entity;
+
+/**
+ * @Description:
+ * @Author: Jungle.Wu
+ * @Date: 2019/10/29
+ * @email: wuyj_prom@si-tech.com.cn
+ */
+@Data
+public class Person {
+    String name;
+    String age;
+
+}
